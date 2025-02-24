dd978668557fea76ecf6533f8fa55756777b86b2
Xr4t0s <nicolastadros.maillol@gmail.com>
2025-02-24 14:00:19 +0100

    Added installation script

---
+++ install.bat
@@ -0,0 +1,45 @@
+@echo off
+setlocal
+
+where python >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Python not found. Installation in progress...
+    powershell -Command "& {Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe' -OutFile 'python-installer.exe'}"
+    start /wait python-installer.exe /quiet InstallAllUsers=1 PrependPath=1
+    del python-installer.exe
+    echo Python installed successfully. Please restart your terminal and run this script again.
+    exit /b
+)
+
+where git >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Git not found. Installing...
+    powershell -Command "& {Invoke-WebRequest -Uri 'https://github.com/git-for-windows/git/releases/latest/download/Git-64-bit.exe' -OutFile 'git-installer.exe'}"
+    start /wait git-installer.exe /VERYSILENT /NORESTART /SP-
+    del git-installer.exe
+    echo Git installed successfully. Please restart your terminal and run this script again.
+    exit /b
+)
+
+powershell -Command "Set-ExecutionPolicy Unrestricted -Scope Process -Force"
+
+set "PATH=%PATH%;%USERPROFILE%\AppData\Local\Programs\Python\Python3*;%USERPROFILE%\AppData\Local\Programs\Python\Python3*\Scripts"
+
+if not exist venv (
+    echo Creating virtual environment...
+    python -m venv venv
+)
+
+call venv\Scripts\activate.bat
+
+python -m ensurepip
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+venv\Scripts\python.exe -m PyInstaller --onefile --name BaseX.exe main.py --clean
+
+echo ✔ Compilation successful! There are 2 files in the dist folder: BaseXSetup and BaseX.
+echo - First, run Setup and enter your Telegram information.
+echo - Then execute BaseX once to create a Telegram link to your account, allowing you to send messages from your computer.
+
+exit /b
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if ! command -v python3 &> /dev/null; then
+    echo "Python3 not found. Installing..."
+    sudo apt update && sudo apt install -y python3 python3-venv python3-pip
+fi
+
+if ! command -v git &> /dev/null; then
+    echo "Git not found. Installing..."
+    sudo apt install -y git
+fi
+
+if [ ! -d "venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv venv
+fi
+
+source venv/bin/activate
+
+python3 -m ensurepip
+python3 -m pip install --upgrade pip
+pip install -r requirements.txt
+
+venv/bin/python3 -m PyInstaller --onefile --name BaseX main.py --clean
+
+echo "✔ Compilation successful! There are 2 files in the dist folder: BaseXSetup and BaseX."
+echo "- First, run Setup and enter your Telegram information."
+echo "- Then execute BaseX once to create a Telegram link to your account, allowing you to send messages from your computer."
